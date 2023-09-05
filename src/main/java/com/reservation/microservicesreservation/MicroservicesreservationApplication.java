94766137fc7cf9ec60f9fb7d8b6c21167937b3d6
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-09-05 15:08:07 +0200

    INITIAL COMMIT

---
+++ src/main/java/com/reservation/microservicesreservation/MicroservicesreservationApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.microservicesreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesreservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reservation/microservicesreservation/MicroservicesreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.microservicesreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
